c6366f954b5661901ab6ab8680193af60dc27f3e
LuiguiHer <hernandez4389562@gmail.com>
2023-02-28 15:01:30 -0500

    Credentials file

---
+++ app/src/main/java/com/example/xlu/ui/utils/Credentials.kt
@@ -0,0 +1,10 @@
+package com.example.xlu.ui.utils
+
+class Credentials {
+    companion object{
+        const val FTP_HOST = "luher.engineer"
+        const val FTP_USER = "xluApp@luher.engineer"
+        const val FTP_PASSWORD = "xluapp2023*"
+        const val FTP_PORT = 21
+    }
+}
